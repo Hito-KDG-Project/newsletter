2ebd19cfbb5922e455fcae31a2ee99211aee66b7
apple-computer-machintosh <hirohito.tanaka1120@gmail.com>
2025-11-29 11:58:54 +0900

    commit0

--- app/components/AuthLinks.tsx
+++ app/components/AuthLinks.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 export function Auth() {
   return (
     <div className="flex gap-4 text-sm">
-      <Link href="auth/login">ログイン</Link>
-      <Link href="auth/logout">ログアウト</Link>
+      <Link href="/auth/login">ログイン</Link>
+      <Link href="/auth/logout">ログアウト</Link>
     </div>
   );
 }
